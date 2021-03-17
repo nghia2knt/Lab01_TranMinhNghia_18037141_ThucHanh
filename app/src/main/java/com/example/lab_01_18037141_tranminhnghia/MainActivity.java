f47c551ce62fc8a23e149307ab501f3acf4f8130
nghia2knt <77620982+nghia2knt@users.noreply.github.com>
2021-03-17 10:41:16 +0700

    First commit

---
+++ app/src/main/java/com/example/lab_01_18037141_tranminhnghia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab_01_18037141_tranminhnghia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_05);
+    }
+}
